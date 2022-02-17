a7bf1699c5e1942ba8636bd2c5a3fc25997a364b
Hardik_5894 <Vaviya.365@gmail.com>
2022-02-17 15:01:30 -0500

    Add project files.

---
+++ DataLayer/DataAccess/EmployeeIO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using DataLayer.Business;
+
+namespace DataLayer.DataAccess
+{
+    public static class EmployeeIO
+    {
+        const string dir = @"";
+
+        const string filepath = dir + "employee.dat";
+
+        public static void SaveRecord(Employee emp)
+        {
+            StreamWriter sw = new StreamWriter(filepath, true);
+            sw.WriteLine(emp.EmployeeId + "," + emp.FirstName + "," + emp.LastName);
+            MessageBox.Show("Data Successfully Saved");
+            sw.Close();
+        }
+
+        public static Employee SearchRecord(int id)
+        {
+            Employee emp = new Employee();
+
+            if (File.Exists(filepath))
+            {
+                StreamReader sr = new StreamReader(filepath);
+                string line = String.Empty;
+                bool found = false;
+
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    string[] fields = line.Split(',');
+                    if (Convert.ToInt32(fields[0]) == id)
+                    {
+                        emp.EmployeeId = Convert.ToInt32(fields[0]);
+                        emp.FirstName = fields[1];
+                        emp.LastName = fields[2];
+
+                        found = true;
+                        break;
+                    }
+                    line = sr.ReadLine();
+
+                }
+                sr.Close();
+
+                if (!found)
+                {
+                    emp = null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Data not found");
+
+            }
+            return emp;
+        }
+
+    }
+}
---
+++ DataLayer/FormDataLayer.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace DataLayer
+{
+    partial class FormDataLayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxEmpID = new System.Windows.Forms.TextBox();
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.textBoxLastname = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EmployeeId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Firstname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lastname";
+            // 
+            // textBoxEmpID
+            // 
+            this.textBoxEmpID.Location = new System.Drawing.Point(85, 95);
+            this.textBoxEmpID.Name = "textBoxEmpID";
+            this.textBoxEmpID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxEmpID.TabIndex = 3;
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.Location = new System.Drawing.Point(256, 95);
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFirstname.TabIndex = 4;
+            // 
+            // textBoxLastname
+            // 
+            this.textBoxLastname.Location = new System.Drawing.Point(420, 95);
+            this.textBoxLastname.Name = "textBoxLastname";
+            this.textBoxLastname.Size = new System.Drawing.Size(100, 22);
+            this.textBoxLastname.TabIndex = 5;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(184, 179);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 6;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(85, 273);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(496, 150);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "EmployeeID";
+            this.columnHeader1.Width = 172;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "FirstName";
+            this.columnHeader2.Width = 171;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "LastName";
+            this.columnHeader3.Width = 147;
+            // 
+            // FormDataLayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 447);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxLastname);
+            this.Controls.Add(this.textBoxFirstname);
+            this.Controls.Add(this.textBoxEmpID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDataLayer";
+            this.Text = "FormDataLayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxEmpID;
+        private System.Windows.Forms.TextBox textBoxFirstname;
+        private System.Windows.Forms.TextBox textBoxLastname;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
